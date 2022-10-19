2059147310b262918d183ff1d593179c5f4279ac
Rafa2v88 <r.rodrigues_g@hotmail.com>
2022-10-19 18:46:39 +0100

    fim

--- pages/doacoes.js
+++ pages/doacoes.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
 import styles from '../styles/doar.module.css'
 import { NavBar } from '../scr/Componentes/navbar'
+import HomePage from '../scr/Componentes/homePage'
 
 export default function Doacoes() {
 
     return (
         <div>
-            <div>
-                <NavBar />
-            </div>
+
+            <NavBar />
+
 
 
 
@@ -18,32 +19,21 @@ export default function Doacoes() {
 
                 <h2>Localização</h2>
                 <img src="/imagens/Mapa.png" />
-
-            </div>
-            <div className={styles.map}>
-                <p> Seus filhos cresceram e já não sabes oque fazer com a roupa? Ajude outras crianças. Doe para nós!!.</p>
+                ©<p> Seus aaaaaaaa filhos cresceram e já não sabes oque fazer com a roupa? Ajude outras crianças. Doe para nós!!.</p>
 
                 <h2>Localização</h2>
                 <img src="/imagens/Mapa.png" />
 
+
+                <HomePage />
             </div>
-            <div className={styles.map}>
-                <p> Seus filhos cresceram e já não sabes oque fazer com a roupa? Ajude outras crianças. Doe para nós!!.</p>
 
-                <h2>Localização</h2>
-                <img src="/imagens/Mapa.png" />
 
-            </div>
-            <div className={styles.map}>
-                <p> Seus filhos cresceram e já não sabes oque fazer com a roupa? Ajude outras crianças. Doe para nós!!.</p>
-
-                <h2>Localização</h2>
-                <img src="/imagens/Mapa.png" />
-
-            </div>
 
 
         </div>
+
+
     )
 
 
