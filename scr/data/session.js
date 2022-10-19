d081a9989e8a339a63b0646997950174ca7b1d32
Bruna <brunaljesus94@gmail.com>
2022-10-19 13:10:03 +0100

    pastas

---
+++ scr/data/session.js
@@ -0,0 +1,27 @@
+const { ObjectId } = require("mongodb")
+
+const { getMongoCollection } = require("./mongodb")
+
+const DB_NAME = "fundacaoRealizar"
+const COLLECTION_NAME = "session"
+
+
+//cria um token
+//(token é basicamente um id criptografado criado pelo sistema para deixar os dados do usuário mais seguro)
+async function addSession(userId) {
+    const collection = await getMongoCollection(DB_NAME, COLLECTION_NAME)
+    return (await collection.insertOne({ userId })).insertedId
+}
+
+//verificar qual a corrêspondencia do token definido
+//caso o token não seja validado, retorna "null"
+async function getSessionByToken(token) {
+    if (!ObjectId.isValid(token)) return null
+    const collection = await getMongoCollection(DB_NAME, COLLECTION_NAME)
+    return await collection.findOne({ _id: new ObjectId(token) })
+}
+
+export {
+    addSession,
+    getSessionByToken
+}
