b6c3379c282fa1851f19d94f93bfa0b2498f5365
Rafa2v88 <r.rodrigues_g@hotmail.com>
2022-10-19 17:37:41 +0100

    att

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import HomePage from '../scr/Componentes/homePage'
+import Doacoes from './doacoes'
+
 import { NavBar } from '../scr/Componentes/navbar'
 import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
 
-    <NavBar />
+    <Doacoes />
 
 
   )
