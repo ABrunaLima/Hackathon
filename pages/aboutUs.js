42c96e8301ecfb7095b28246a7fb5b549317b6a5
Rafael <r.rodrigues_g@hotmail.com>
2022-10-20 00:55:46 +0100

    fim

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -29,21 +29,5 @@ export default function AboutUs() {
     )
 }
 
-import { NavBar } from "../scr/Componentes/navbar"
-
-
-export default function SobreNos() {
-    return (
-        <div>
-            <div>
-                <NavBar />
-            </div>
-            <div >
-                <h1>about us</h1>
-                <p>A fundaçao Realizar foi criada em 2020 com o objetivo de ajudar famílias carenciadas através da distribuição de roupa para crianças que nos foi doada. A nivel nacional a maior parte das doações é feita para a grande Lisboa, mas não recusamos pedidos de ajuda de outras regiões do país. A nossa missão é ajudar quem precisa para a melhoria das condições de vida dos mais vulneráveis.</p>
-            </div>
-        </div>
-        )
-}
 
 
