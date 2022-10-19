ac228ac017fd5b8c17bbf95403a9b0e9f4189f5e
luis <luis98lffc@gmail.com>
2022-10-19 17:50:14 +0100

    formulario

--- scr/Componentes/homePage.js
+++ scr/Componentes/homePage.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import styles from '../../styles/HomePage.module.css'
 
 export default function HomePage() {
-    const [name, setName] = useState('')
+    const [nome, setNome] = useState('')
     const [telefone, setTelefone] = useState('')
     const [morada, setMorada] = useState('')
     const [sexo1, setSexo1] = useState('')
@@ -23,7 +23,7 @@ export default function HomePage() {
     const handleFormSubmit = (event) => {
         event.preventDefault()
         const dataToSubmit = {
-            name: name,
+            nome: nome,
             telefone: telefone,
             morada: morada,
             sexo1: sexo1,
@@ -37,47 +37,89 @@ export default function HomePage() {
 
     return (
 
+
         <div className={styles.container}>
-            <h1>Formulário</h1>
+            <h1>Se necessita de roupa preencha este formulario</h1>
+
             <br />
-            <form onSubmit={handleFormSubmit}>
-                <label>Name :
-                    <input type='text' value={name} name='name' onChange={(event) => setName(event.target.value)}></input>
-                </label>
-                <br />
-                <label>Telefone :
-                    <input type='text' value={telefone} name='telefone' onChange={(event) => setFone(event.target.value)}></input>
-                </label>
-                <br />
-                <label>Morada:
-                    <input type='text' value={morada} name='morada' onChange={(event) => setMorada(event.target.value)}></input>
-                </label>
-                <br />
-                Filho 1
-                <br />
-                <label>Sexo:
-                    <input type='text' value={sexo1} name='sexo1' onChange={(event) => setSexo1(event.target.value)}></input>
-                </label>
-                <br />
-                <label>Tamanho:
-                    <input type='text' value={tamanho1} name='tamanho1' onChange={(event) => setTamanho1(event.target.value)}></input>
-                </label>
-                <br />
-                Filho 2
-                <br />
-                <label>Sexo:
-                    <input type='text' value={sexo2} name='sexo2' onChange={(event) => setSexo2(event.target.value)}></input>
-                </label>
-                <br />
-                <label>Tamanho:
-                    <input type='text' value={tamanho2} name='tamanho2 ' onChange={(event) => setTamanho2(event.target.value)}></input>
-                </label>
-                <br />
-                <button type={'submit'} className={styles.button}></button>
-            </form>
+            <div className={styles.formulario}>
+                <form onSubmit={handleFormSubmit}>
+                    <label className={styles.label}>
+                        <span>Nome :</span>
+                        <input type='text' value={nome} name='nome' onChange={(event) => setNome(event.target.value)}></input>
+                    </label>
+                    <br />
+                    <br />
+                    <label className={styles.label}>Telefone :
+                        <input type='text' value={telefone} name='telefone' onChange={(event) => setTelefone(event.target.value)}></input>
+                    </label>
+                    <br />
+                    <br />
+                    <label className={styles.label}>Morada:
+                        <input type='text' value={morada} name='morada' onChange={(event) => setMorada(event.target.value)}></input>
+                    </label>
+                    <br />
+                    <br />
+                    Filho 1
+                    <br />
+                    <label className={styles.label}>Genero:
+                        <input type="radio" name="genero" value="Masculino" /> Masculino<br />
+                        <input type="radio" name="genero" value="Feminino" /> Feminino<br />
+                    </label>
+                    <br />
+                    <br />
+                    <label className={styles.label}>Tamanho:
+                        < select type='text' value={tamanho1} name='tamanho1' onChange={(event) => setTamanho1(event.target.value)}>
+                            <option value="0-6 Meses ">0-6 Meses </option>
+                            <option value="6-12 Meses ">6-12 Meses </option>
+                            <option value="1 Anos ">1 Anos </option>
+                            <option value="2 Anos ">2 Anos </option>
+                            <option value="3 Anos ">3 Anos </option>
+                            <option value="4 Anos ">4 Anos </option>
+                            <option value="5 Anos ">5 Anos </option>
+                            <option value="6 Anos ">6 Anos </option>
+                            <option value="7 Anos ">7 Anos </option>
+                            <option value="8 Anos ">8 Anos </option>
+                            <option value="9 Anos ">9 Anos </option>
+                            <option value="10 Anos ">10 Anos </option>
+                            <option value="11-12 Anos ">11-12 Anos </option>
+                            <option value="13-14 Anos ">13-14 Anos </option>
+                        </select>
+                    </label>
+                    <br />
+                    Filho 2
+                    <br />
+                    <label className={styles.label}>Genero:
+                        <input type="radio" name="genero" value="Masculino" /> Masculino<br />
+                        <input type="radio" name="genero" value="Feminino" /> Feminino<br />
+                    </label>
+                    <br />
+                    <br />
+                    <label className={styles.label}>Tamanho:
+                        <select type='text' value={tamanho2} name='tamanho2 ' onChange={(event) => setTamanho2(event.target.value)}>
+                            <option value="0-6 Meses ">0-6 Meses </option>
+                            <option value="6-12 Meses ">6-12 Meses </option>
+                            <option value="1 Anos ">1 Anos </option>
+                            <option value="2 Anos ">2 Anos </option>
+                            <option value="3 Anos ">3 Anos </option>
+                            <option value="4 Anos ">4 Anos </option>
+                            <option value="5 Anos ">5 Anos </option>
+                            <option value="6 Anos ">6 Anos </option>
+                            <option value="7 Anos ">7 Anos </option>
+                            <option value="8 Anos ">8 Anos </option>
+                            <option value="9 Anos ">9 Anos </option>
+                            <option value="10 Anos ">10 Anos </option>
+                            <option value="11-12 Anos ">11-12 Anos </option>
+                            <option value="13-14 Anos ">13-14 Anos </option>
+
+                        </select>
+                    </label>
+                    <br />
+                    <button type={'submit'} className={styles.button}>Submeter</button>
+                </form>
 
 
-
+            </div>
         </div>
 
 
