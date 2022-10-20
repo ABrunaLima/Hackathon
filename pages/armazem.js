18c477d40eb15d4e9afa55341eacd082ddf9b688
Luis <luis98lffc@gmail.com>
2022-10-20 01:38:44 +0100

    pedidos

---
+++ pages/armazem.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/armazem.module.css"
+import NavBarAdmin from "../scr/Componentes/navbaradmin";
+
+export default function Armazem() {
+
+
+    return (
+    
+    <div className={styles.armazem}>
+        <div>
+          <NavBarAdmin/>
+            </div>
+            <div className={styles.filtros}>
+lal
+            </div>
+
+            </div>
+    )
+}
